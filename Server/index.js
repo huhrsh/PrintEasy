5d82c51195f6b8f43dd83e99b4463432aaa8a84f
Harsh Jain <122169261+huhrsh@users.noreply.github.com>
2024-02-07 23:52:02 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -36,7 +36,8 @@ app.use(session({
         maxAge:(1000*60*60*24*31)     
     },
     store:MongoStore.create({
-        mongoUrl:'mongodb+srv://harshj2010:W6k2pwIcJ9MdircD@cluster0.nynygsd.mongodb.net/?retryWrites=true&w=majority',
+        // mongoUrl:'mongodb+srv://harshj2010:W6k2pwIcJ9MdircD@cluster0.nynygsd.mongodb.net/?retryWrites=true&w=majority',
+        mongoUrl: "mongodb+srv://harshj2010:W6k2pwIcJ9MdircD@cluster0.nynygsd.mongodb.net/?retryWrites=true&w=majority"
         autoRemove:'disabled'
     })
 }))
